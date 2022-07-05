b015c0a37336781b3ec2622e1b448cb8c163afb5
hsgill3106 <108752326+hsgill3106@users.noreply.github.com>
2022-07-05 12:32:34 -0600

    JS code complete

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("hi");
+//click counter by Mr. V
+
+//GLOBAL VARIABLE
+let count = 0;
+
+document.addEventListener("click", clickCount);
+
+//EVENT FUNCTION
+function clickCount() {
+count++;
+document.getElementById("count-display").innerHTML = count;
+}
